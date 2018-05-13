5303346af33bc8a7aa6b37b60a0a0141b83c93ae
marioAUPM <mario.arroyo.benito@alumnos.upm.es>
2018-05-13 18:03:12 +0200

    layout

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var favicon = require('serve-favicon');
+var partials = require('express-partials');
 
 var indexRouter = require('./routes/index');
 
@@ -12,6 +13,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(partials());
+
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json());
