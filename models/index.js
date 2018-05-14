0ec89eba3e65e1fa4ed430ae2dfd9a4acc926524
marioAUPM <mario.arroyo.benito@alumnos.upm.es>
2018-05-14 09:14:12 +0200

    heroku postgress

--- models/index.js
+++ models/index.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 const Sequelize = require('sequelize');
-const sequelize  = new Sequelize("sqlite:quiz.sqlite");
+
+const url = process.env.DATABASE_URL || 'sqlite:quiz.sqlite';
+
+const sequelize  = new Sequelize(url);
 sequelize.import(path.join(__dirname,'quiz'));
 sequelize.import(path.join(__dirname,'session'));
 
